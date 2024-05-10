993b573e2662b34768a25915ff8a6ba7341ca499
IgnacioPardo <ignacio.pardo@ort.edu.ar>
2024-05-10 18:13:09 -0300

    Update mercadopago.sql

--- mercadopago.sql
+++ mercadopago.sql
@@ -61,6 +61,23 @@ BEFORE INSERT ON Tarjeta
 FOR EACH ROW
 EXECUTE FUNCTION check_cu_virtual();
 
+CREATE TABLE Rendimiento (
+    id SERIAL PRIMARY KEY,
+    fecha_pago DATE,
+    comienzo_plazo DATE NOT NULL,
+    fin_plazo DATE,
+    TNA FLOAT,
+    monto FLOAT
+);
+
+CREATE TABLE RendimientoUsuario (
+    clave_uniforme VARCHAR(50),
+    id INTEGER,
+    PRIMARY KEY (clave_uniforme, id),
+    FOREIGN KEY (clave_uniforme) REFERENCES Clave(clave_uniforme),
+    FOREIGN KEY (id) REFERENCES Rendimiento(id)
+);
+
 CREATE TABLE Transaccion (
     codigo SERIAL PRIMARY KEY,
     CU_Origen VARCHAR(50),
@@ -77,22 +94,52 @@ CREATE TABLE Transaccion (
     FOREIGN KEY (numero) REFERENCES Tarjeta(numero)
 );
 
-CREATE TABLE Rendimiento (
-    id SERIAL PRIMARY KEY,
-    fecha_pago DATE,
-    comienzo_plazo DATE,
-    fin_plazo DATE,
-    TNA FLOAT,
-    monto FLOAT
-);
+CREATE OR REPLACE FUNCTION check_balance()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (SELECT saldo FROM Usuarios WHERE clave_uniforme = NEW.CU_Origen) < NEW.monto THEN
+        RAISE EXCEPTION 'Not enough balance';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
-CREATE TABLE RendimientoUsuario (
-    clave_uniforme VARCHAR(50),
-    id INTEGER,
-    PRIMARY KEY (clave_uniforme, id),
-    FOREIGN KEY (clave_uniforme) REFERENCES Clave(clave_uniforme),
-    FOREIGN KEY (id) REFERENCES Rendimiento(id)
-);
+-- Para el usuario 2:
+-- depositar 1000, 
+-- comenzar un rendimiento
+-- interrumpirlo transfiriendole 500 al usuario 1 (Agregar un trigger para que cuando se cree una transacción, si el usuario tiene un rendimiento activo, se finalice y se cree uno nuevo con el monto restante pero que no se pague)
+-- continuar el rendimiento y pagar los rendimientos
+
+CREATE OR REPLACE FUNCTION check_rendimiento()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Checkear si el usuario tiene un rendimiento activo, es decir sin fecha de fin
+    -- Joinear RendimientoUsuario con Rendimiento para obtener el id del rendimiento activo
+    IF EXISTS (
+        SELECT * FROM RendimientoUsuario JOIN Rendimiento ON RendimientoUsuario.id = Rendimiento.id WHERE clave_uniforme = NEW.CU_Origen AND fin_plazo IS NULL
+    ) THEN
+        
+        -- Finalizar el rendimiento
+        UPDATE Rendimiento SET fin_plazo = NEW.fecha WHERE id = (SELECT id FROM RendimientoUsuario WHERE clave_uniforme = NEW.CU_Origen AND fin_plazo IS NULL);
+        -- Crear un nuevo rendimiento con el monto restante
+        INSERT INTO Rendimiento (fecha_pago, comienzo_plazo, TNA, monto) VALUES (NEW.fecha, NEW.fecha, 0.1, (SELECT saldo FROM Usuarios WHERE clave_uniforme = NEW.CU_Origen) - NEW.monto);
+        INSERT INTO RendimientoUsuario (clave_uniforme, id) VALUES (NEW.CU_Origen, (SELECT id FROM Rendimiento WHERE fecha_pago = NEW.fecha AND comienzo_plazo = NEW.fecha AND TNA = 0.1 AND monto = (SELECT saldo FROM Usuarios WHERE clave_uniforme = NEW.CU_Origen) - NEW.monto));
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_rendimiento_trigger
+BEFORE INSERT ON Transaccion
+FOR EACH ROW
+WHEN (NEW.es_con_tarjeta = FALSE)
+EXECUTE FUNCTION check_rendimiento();
+
+CREATE TRIGGER check_balance_trigger
+BEFORE INSERT ON Transaccion
+FOR EACH ROW
+WHEN (NEW.es_con_tarjeta = FALSE)
+EXECUTE FUNCTION check_balance();
 
 CREATE TABLE TransaccionTarjeta (
     codigo INTEGER,
